f48de4df26aaf4b74cdc453ba9a97839aac06425
Sky Rose <srose@mbta.com>
2024-02-26 16:58:40 -0500

    remove uneeded config for Endpoint server: true

--- config/runtime.exs
+++ config/runtime.exs
@@ -6,20 +6,6 @@ import Config
 # and secrets from environment variables or elsewhere. Do not define
 # any compile-time configuration in here, as it won't be applied.
 # The block below contains prod specific runtime configuration.
-
-# ## Using releases
-#
-# If you use `mix release`, you need to explicitly enable the server
-# by passing the PHX_SERVER=true when you start it:
-#
-#     PHX_SERVER=true bin/orbit start
-#
-# Alternatively, you can use `mix phx.gen.release` to generate a `bin/server`
-# script that automatically sets the env var above.
-if System.get_env("PHX_SERVER") do
-  config :orbit, OrbitWeb.Endpoint, server: true
-end
-
 if config_env() == :prod do
   maybe_ipv6 = if System.get_env("ECTO_IPV6") in ~w(true 1), do: [:inet6], else: []
 
