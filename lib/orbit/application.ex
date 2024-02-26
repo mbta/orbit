fa97249f1db9c307ee4cc1483012bcf8fd14bc6f
Sky Rose <srose@mbta.com>
2024-02-26 15:01:51 -0500

    refactor: clean up application supervisor

--- lib/orbit/application.ex
+++ lib/orbit/application.ex
@@ -15,10 +15,7 @@ defmodule Orbit.Application do
 
     :ok = Logster.attach_phoenix_logger()
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: Orbit.Supervisor]
-    Supervisor.start_link(children, opts)
+    Supervisor.start_link(children, strategy: :one_for_one, name: Orbit.Supervisor)
   end
 
   # Tell Phoenix to update the endpoint configuration
