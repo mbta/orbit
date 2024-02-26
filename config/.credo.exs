645c5b36e0d8aa97ade8624cda8b20e2af46fe5c
Sky Rose <srose@mbta.com>
2024-02-26 17:21:43 -0500

    chore: quiet credo rule

--- config/.credo.exs
+++ config/.credo.exs
@@ -86,8 +86,7 @@
           #
           ## Design Checks
           #
-          {Credo.Check.Design.AliasUsage,
-           [if_nested_deeper_than: 2, if_called_more_often_than: 1]},
+          {Credo.Check.Design.AliasUsage, false},
           {Credo.Check.Design.TagFIXME, []},
           {Credo.Check.Design.TagTODO, false},
 
