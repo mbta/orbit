0395f2175c141a96f1c2ef211150bef5d119e2c1
Sky Rose <srose@mbta.com>
2024-05-13 13:39:38 -0400

    test

---
+++ test/orbit_web/controllers/health_controller_test.exs
@@ -0,0 +1,9 @@
+
+defmodule OrbitWeb.HealthControllerTest do
+  use OrbitWeb.ConnCase
+
+  test "GET /_health", %{conn: conn} do
+    conn = get(conn, ~p"/_health")
+    assert response(conn, 200)
+  end
+end
