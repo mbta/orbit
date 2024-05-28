2cc905d67c3e85390adef247c67d867825132d8b
Sky Rose <srose@mbta.com>
2024-05-28 17:11:33 -0400

    eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,25 @@
-import globals from "globals";
+// @ts-check
+
 import pluginJs from "@eslint/js";
+// @ts-expect-error couldn't find types for this package
+import pluginReactConfig from "eslint-plugin-react/configs/jsx-runtime.js";
+import globals from "globals";
 import tseslint from "typescript-eslint";
-import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
 
-
-export default [
-  {languageOptions: { globals: globals.browser }},
+export default tseslint.config(
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReactConfig,
-];
+  {
+    ignores: ["*", "!js/", "!*.ts", "!*.mjs"],
+  },
+  {
+    name: "orbit",
+    linterOptions: {
+      reportUnusedDisableDirectives: "warn",
+    },
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
+);
