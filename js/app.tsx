ce3baf5e9324ba0e072bcf94b4f4ce09d1ff69db
Preston Mueller <pmueller@mbta.com>
2024-05-31 15:37:46 -0400

    fix: @lemald feedback

--- js/app.tsx
+++ js/app.tsx
@@ -2,5 +2,5 @@ import "./phoenix.js";
 import { App } from "./components/app.js";
 import { createRoot } from "react-dom/client";
 
-const root = createRoot(document.getElementById("app") as Element);
+const root = createRoot(document.getElementById("app")!);
 root.render(<App />);
