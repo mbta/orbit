51e0ebb0f55ed6d9a65f6ed65db42e42846d7dfb
Preston Mueller <pmueller@mbta.com>
2024-05-31 16:19:22 -0400

    fix: override no-non-null-assertion

--- js/app.tsx
+++ js/app.tsx
@@ -2,5 +2,6 @@ import "./phoenix.js";
 import { App } from "./components/app.js";
 import { createRoot } from "react-dom/client";
 
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const root = createRoot(document.getElementById("app")!);
 root.render(<App />);
