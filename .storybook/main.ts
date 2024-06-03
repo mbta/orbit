437b0150985cf1b6f293b3accb9995918a825c29
Preston Mueller <pmueller@mbta.com>
2024-06-03 17:04:38 -0400

    feat(dev): Add storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,33 @@
+import type { StorybookConfig } from "@storybook/react-vite";
+
+const config: StorybookConfig = {
+  stories: [
+    "../stories/**/*.mdx",
+    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+  ],
+  addons: [
+    "@storybook/addon-onboarding",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        cssLoaderOptions: {
+          // When you have splitted your css over multiple files
+          // and use @import('./other-styles.css')
+          importLoaders: 1,
+        },
+        postcssLoaderOptions: {
+          // When using postCSS 8
+          implementation: require('postcss'),
+        },
+      },
+    }
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+};
+export default config;
---
+++ stories/home.stories.tsx
@@ -0,0 +1,14 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Home } from "../js/components/home"
+
+const meta: Meta<typeof Home> = {
+  component: Home,
+};
+
+export default meta;
+type Story = StoryObj<typeof Home>;
+
+export const Primary: Story = {
+  args: {
+  },
+};
