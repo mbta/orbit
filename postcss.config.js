71dcbcb1e9886edafb7e43b61d27a16b3bb77a64
Preston Mueller <pmueller@mbta.com>
2024-06-03 17:46:59 -0400

    Clarify comment

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,5 @@
 // STORYBOOK ONLY!
-// This is only in use by Vite for Storybook
+// This is only in use by postcss (via Vite) for Storybook
 module.exports = {
     plugins: {
       tailwindcss: {},
