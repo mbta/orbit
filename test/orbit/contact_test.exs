4c7cefd75d428fcfe511793287a11f038d7bf27f
Preston Mueller <pmueller@mbta.com>
2024-06-05 15:31:38 -0400

    test: cannot insert badge number with leading zero

--- test/orbit/contact_test.exs
+++ test/orbit/contact_test.exs
@@ -10,4 +10,15 @@ defmodule Orbit.ContactTest do
       badge_number: "123456789"
     })
   end
+
+  test "cannot insert a contact with a leading zero" do
+    assert_raise Ecto.ConstraintError,
+                 fn ->
+                   Repo.insert!(%Contact{
+                     first_name: "Arthur",
+                     last_name: "Read",
+                     badge_number: "0123456789"
+                   })
+                 end
+  end
 end
