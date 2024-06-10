c1f80e70eb0317ce15bdece9a70a92c8dbd13d4a
Preston Mueller <pmueller@mbta.com>
2024-06-10 15:04:59 -0400

    fix: remove 'remembers target URL' test

--- test/orbit_web/controllers/auth_controller_test.exs
+++ test/orbit_web/controllers/auth_controller_test.exs
@@ -20,15 +20,6 @@ defmodule OrbitWeb.AuthControllerTest do
       assert redirected_to(conn) == "/"
     end
 
-    # test "remembers target URL", %{conn: conn} do
-    #   conn = get(conn, "/some/internal/page")
-    #   assert redirected_to(conn) == ~p"/login"
-
-    #   conn = login_with_groups(conn, [])
-
-    #   assert redirected_to(conn) == "/some/internal/page"
-    # end
-
     @tag capture_log: true
     test "invalid credentials redirect to /login", %{conn: conn} do
       conn = get(conn, ~p"/auth/keycloak/callback?#{%{"invalid" => "true"}}")
