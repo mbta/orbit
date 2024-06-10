c82fc629ee6c8ea3d347d1e3022a413452463431
Preston Mueller <pmueller@mbta.com>
2024-06-10 15:10:02 -0400

    fix: Add 'remembers target URL' test back

--- test/orbit_web/controllers/auth_controller_test.exs
+++ test/orbit_web/controllers/auth_controller_test.exs
@@ -20,6 +20,15 @@ defmodule OrbitWeb.AuthControllerTest do
       assert redirected_to(conn) == "/"
     end
 
+    test "remembers target URL", %{conn: conn} do
+      conn = get(conn, "/help")
+      assert redirected_to(conn) == ~p"/login"
+
+      conn = login_with_groups(conn, [])
+
+      assert redirected_to(conn) == "/help"
+    end
+
     @tag capture_log: true
     test "invalid credentials redirect to /login", %{conn: conn} do
       conn = get(conn, ~p"/auth/keycloak/callback?#{%{"invalid" => "true"}}")
