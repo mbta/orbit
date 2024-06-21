e2916b499f2fb99fea823a2016d33b6a5691e766
Preston Mueller <pmueller@mbta.com>
2024-06-21 10:34:48 -0400

    debug: log KEYCLOAK_CLIENT_SECRET length

--- lib/orbit_web/controllers/auth_controller.ex
+++ lib/orbit_web/controllers/auth_controller.ex
@@ -1,6 +1,7 @@
 defmodule OrbitWeb.AuthController do
   use OrbitWeb, :controller
   plug(Ueberauth)
+  require Logger
 
   alias OrbitWeb.Auth.Auth
 
@@ -16,6 +17,10 @@ defmodule OrbitWeb.AuthController do
   def request(conn, _params) do
     # unrecognized provider
     # (recognized providers are caught by Ueberauth before this point)
+    Logger.warning(
+      "Keycloak client secret length: #{String.length(System.fetch_env!("KEYCLOAK_CLIENT_SECRET"))}"
+    )
+
     conn
     |> put_status(:bad_request)
     |> text("Bad Request (unrecognized provider)")
