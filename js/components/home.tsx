6fc1d5c131ed4825309afa05005ee4a710f51610
Eddie Maldonado <emaldonado@mbta.com>
2024-06-26 11:04:50 -0400

    feat: basic modal on landing page for OSI

--- js/components/home.tsx
+++ js/components/home.tsx
@@ -1,12 +1,16 @@
+import { OperatorSignInModal } from "./operatorSignIn/operatorSignInModal";
 import { ReactElement } from "react";
 
 export const Home = (): ReactElement => {
   return (
-    <div className="text-3xl">
-      🪐
-      <span className="text-mbta-orange">O</span>
-      <span className="text-mbta-red">r</span>
-      <span className="text-mbta-blue">b</span>it
-    </div>
+    <>
+      <div className="text-3xl">
+        🪐
+        <span className="text-mbta-orange">O</span>
+        <span className="text-mbta-red">r</span>
+        <span className="text-mbta-blue">b</span>it
+      </div>
+      <OperatorSignInModal />
+    </>
   );
 };
