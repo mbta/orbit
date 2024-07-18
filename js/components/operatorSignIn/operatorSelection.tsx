0d98c1f3f1ed7e8586c2cfed276decf8fd389120
Preston Mueller <pmueller@mbta.com>
2024-07-18 15:45:19 -0400

    fix: nfc signins get 'nfc' method (#46)

--- js/components/operatorSignIn/operatorSelection.tsx
+++ js/components/operatorSignIn/operatorSelection.tsx
@@ -3,6 +3,7 @@ import { findEmployeeByBadgeSerial } from "../../hooks/useEmployees";
 import { useNfc } from "../../hooks/useNfc";
 import { Employee } from "../../models/employee";
 import { className } from "../../util/dom";
+import { BadgeEntry } from "./types";
 import { ReactElement, useEffect, useId, useState } from "react";
 
 export const OperatorSelection = ({
@@ -11,11 +12,11 @@ export const OperatorSelection = ({
   employees,
 }: {
   nfcSupported: boolean;
-  onOK: (badge: string) => void;
+  onOK: (badgeEntry: BadgeEntry) => void;
   employees: ApiResult<Employee[]>;
 }): ReactElement => {
   const inputId = useId();
-  const [badgeEntry, setBadgeEntry] = useState<string>("");
+  const [badgeEntry, setBadgeEntry] = useState<BadgeEntry | null>(null);
 
   const { result: nfcResult } = useNfc();
 
@@ -27,14 +28,14 @@ export const OperatorSelection = ({
       )?.badge;
 
       if (badge) {
-        onOK(badge);
+        onOK({ number: badge, method: "nfc" });
       } else {
         // handle case where we can't find operator for badge
       }
     }
   }, [nfcResult, employees, onOK]);
 
-  const buttonEnabled = badgeEntry !== "";
+  const buttonEnabled = badgeEntry !== null;
 
   return (
     <div className="flex flex-col content-stretch">
@@ -47,12 +48,19 @@ export const OperatorSelection = ({
         id={inputId}
         inputMode="numeric"
         onChange={(evt) => {
-          setBadgeEntry(evt.target.value);
+          setBadgeEntry({ number: evt.target.value, method: "manual" });
         }}
       />
       <button
         disabled={!buttonEnabled}
         onClick={() => {
+          // This should never happen, but throw just in case.
+          if (badgeEntry === null) {
+            throw new Error(
+              "operatorSelection badgeEntry was impossibly null.",
+            );
+          }
+
           onOK(badgeEntry);
         }}
         className={className([
