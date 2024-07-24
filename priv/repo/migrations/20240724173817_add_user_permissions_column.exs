c4b5d3977d1381c8c069f80a03d2eebe169a0af7
Eddie Maldonado <emaldonado@mbta.com>
2024-07-24 13:53:36 -0400

    feat: migration to add column

---
+++ priv/repo/migrations/20240724173817_add_user_permissions_column.exs
@@ -0,0 +1,14 @@
+defmodule Orbit.Repo.Migrations.AddUserPermissionsColumn do
+  use Ecto.Migration
+
+  def change do
+    execute(
+      "CREATE TYPE user_permission AS ENUM ('operator_sign_in')",
+      "DROP TYPE user_permission"
+    )
+
+    alter table("users") do
+      add :permissions, {:array, :user_permission}
+    end
+  end
+end
