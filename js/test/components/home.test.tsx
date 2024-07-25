271451fa7de0ee533861ea76e69f26982bfe2443
Sky Rose <srose@mbta.com>
2024-07-25 16:15:22 -0400

    test: can open modal from landing page

--- js/test/components/home.test.tsx
+++ js/test/components/home.test.tsx
@@ -1,14 +1,19 @@
 import { Home } from "../../components/home";
 import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { MemoryRouter } from "react-router-dom";
 
 describe("home", () => {
-  test("loads orbit placeholder", () => {
+  test("can open modal", async () => {
     const view = render(
       <MemoryRouter>
         <Home />
       </MemoryRouter>,
     );
-    expect(view.getAllByText(/sign.?in/i)).not.toBeEmpty();
+    expect(view.queryByText(/fit for duty/i)).not.toBeInTheDocument();
+    await userEvent.click(
+      view.getByRole("button", { name: "Sign In Operator" }),
+    );
+    expect(view.getByText(/fit for duty/i)).toBeInTheDocument();
   });
 });
