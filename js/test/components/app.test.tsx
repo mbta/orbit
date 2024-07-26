a4fc5f118854df7ac89fefe84860339aae26ba6e
Sky Rose <srose@mbta.com>
2024-07-26 15:54:16 -0400

    fix failing test

--- js/test/components/app.test.tsx
+++ js/test/components/app.test.tsx
@@ -1,15 +1,15 @@
+import { fetch } from "../../browser";
 import { App } from "../../components/app";
 import { render } from "@testing-library/react";
 
 describe("App", () => {
   test("error boundary renders on error", () => {
-    jest.mock("../../components/operatorSignIn/operatorSignInModal", () => ({
-      OperatorSignInModal: () => {
-        throw new Error(
-          "Ah, Houston, we've had a problem. We've had a Main B Bus Undervolt.",
-        );
-      },
-    }));
+    // Cause an error to be thrown anywhere in the app
+    (fetch as jest.MockedFn<typeof fetch>).mockImplementation(() => {
+      throw new Error(
+        "Ah, Houston, we've had a problem. We've had a Main B Bus Undervolt.",
+      );
+    });
 
     jest.spyOn(console, "error").mockImplementation(() => {});
     const view = render(<App />);
