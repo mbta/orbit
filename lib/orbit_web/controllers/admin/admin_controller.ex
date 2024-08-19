dca0ef8d2c2d036ad5c22a69c7d5e9493e1d87b7
Sky Rose <srose@mbta.com>
2024-08-19 11:14:07 -0400

    feat: admin rfid data form saves data

--- lib/orbit_web/controllers/admin/admin_controller.ex
+++ lib/orbit_web/controllers/admin/admin_controller.ex
@@ -1,5 +1,10 @@
 defmodule OrbitWeb.Admin.AdminController do
   use OrbitWeb, :controller
+  import Ecto.Query
+
+  alias Orbit.Employee
+  alias Orbit.BadgeSerial
+  alias Orbit.Repo
 
   plug(OrbitWeb.Plugs.RequireAdmin)
 
@@ -9,7 +14,35 @@ defmodule OrbitWeb.Admin.AdminController do
   end
 
   @spec post_rfid(Plug.Conn.t(), map()) :: Plug.Conn.t()
+  def post_rfid(conn, %{
+        "badge_number" => badge_number,
+        "badge_serial" => badge_serial
+      })
+      when is_binary(badge_number) and
+             badge_number != "" and
+             is_binary(badge_serial) and
+             badge_serial != "" do
+    employee = Repo.one(from e in Employee, where: e.badge_number == ^badge_number)
+
+    if employee != nil do
+      %BadgeSerial{
+        employee: employee,
+        badge_serial: badge_serial
+      }
+      |> BadgeSerial.changeset()
+      |> Repo.insert!()
+
+      text(conn, "OK")
+    else
+      conn
+      |> put_status(400)
+      |> text("Error: no employee with badge_number #{badge_number}")
+    end
+  end
+
   def post_rfid(conn, _params) do
     conn
+    |> put_status(400)
+    |> text("Error: need params `badge_number` and `badge_serial`")
   end
 end
