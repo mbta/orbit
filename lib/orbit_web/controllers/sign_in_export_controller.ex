4ed26a2c4611cf64009286a39ad2db9923fe3498
Eddie Maldonado <emaldonado@mbta.com>
2024-08-29 13:02:20 -0400

    feat: basic CSV data in export

--- lib/orbit_web/controllers/sign_in_export_controller.ex
+++ lib/orbit_web/controllers/sign_in_export_controller.ex
@@ -1,12 +1,63 @@
 defmodule OrbitWeb.SignInExportController do
   use OrbitWeb, :controller
+  alias Orbit.Employee
+  alias Orbit.OperatorSignIn
+  alias Orbit.Repo
+
+  import Ecto.Query
+
+  @timezone Application.compile_env!(:orbit, :timezone)
 
   @spec get(Plug.Conn.t(), map()) :: Plug.Conn.t()
-  def get(conn, %{"date" => _date_string}) do
-    csv = ""
+  def get(conn, %{"date" => date_string}) do
+    case Date.from_iso8601(date_string) do
+      {:ok, date} ->
+        conn
+        |> put_resp_content_type("application/csv")
+        |> send_resp(200, fetch_sign_ins_csv(date))
 
-    conn
-    |> put_resp_content_type("application/csv")
-    |> send_resp(200, csv)
+      {:error, _} ->
+        conn
+        |> put_status(:bad_request)
+        |> text("Bad Request")
+    end
+  end
+
+  @spec fetch_sign_ins_csv(Date.t()) :: String.t()
+  defp fetch_sign_ins_csv(service_date) do
+    {start_datetime, end_datetime} = Util.Time.service_date_boundaries(service_date)
+
+    rows =
+      Repo.all(
+        from osi in OperatorSignIn,
+          where: ^start_datetime < osi.signed_in_at and osi.signed_in_at < ^end_datetime,
+          join: e in assoc(osi, :signed_in_employee),
+          join: u in assoc(osi, :signed_in_by_user),
+          preload: [signed_in_employee: e, signed_in_by_user: u]
+      )
+      |> Enum.map(
+        &%{
+          time: &1.signed_in_at |> DateTime.shift_zone!(@timezone) |> DateTime.to_iso8601(),
+          location: "Orient Heights",
+          signer_badge: &1.signed_in_employee.badge_number,
+          signer_name: Employee.display_name(&1.signed_in_employee),
+          method: &1.sign_in_method,
+          text_version: 1
+        }
+      )
+
+    rows
+    |> CSV.encode(
+      headers: [
+        time: "Time",
+        location: "Location",
+        signer_badge: "Signer Badge #",
+        signer_name: "Signer Name",
+        method: "Method",
+        text_version: "Text Version"
+      ],
+      delimiter: "\n"
+    )
+    |> Enum.join()
   end
 end
--- test/orbit_web/controllers/sign_in_export_controller_test.exs
+++ test/orbit_web/controllers/sign_in_export_controller_test.exs
@@ -1,11 +1,52 @@
 defmodule OrbitWeb.SignInExportControllerTest do
   use OrbitWeb.ConnCase
 
+  import Orbit.Factory
+
   describe "get/2" do
     test "unauthenticated requests get redirected to login", %{conn: conn} do
       conn = get(conn, ~p"/sign-in-export", %{"date" => "2024-08-28"})
 
       assert redirected_to(conn) == ~p"/login"
+    end
+
+    @tag :authenticated
+    test "returns CSV export", %{conn: conn} do
+      {:ok, signed_in_at, _offset} = DateTime.from_iso8601("2024-08-28T17:00:00-04:00")
+
+      insert(:operator_sign_in, %{signed_in_at: signed_in_at})
+
+      conn =
+        get(conn, ~p"/sign-in-export", %{
+          "date" => signed_in_at |> DateTime.to_date() |> Date.to_string()
+        })
+
+      csv = response(conn, :ok)
+
+      result =
+        csv
+        |> String.splitter("\n", trim: true)
+        |> Stream.map(&"#{&1}\n")
+        |> CSV.decode!(headers: true)
+        |> Enum.to_list()
+
+      assert [
+               %{
+                 "Location" => "Orient Heights",
+                 "Method" => "manual",
+                 "Signer Badge #" => "employee_badge0",
+                 "Signer Name" => "Preferredy Person",
+                 "Text Version" => "1",
+                 "Time" => "2024-08-28T17:00:00-04:00"
+               }
+             ] = result
+    end
+
+    @tag :authenticated
+    test "returns a bad request response for a malformed date", %{conn: conn} do
+      conn = get(conn, ~p"/sign-in-export", %{"date" => "invalid"})
+
+      assert text_response(conn, :bad_request)
     end
   end
 end
