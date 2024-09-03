16291ed7c47156482cf5a7007f0d761eb5314769
Sky Rose <srose@mbta.com>
2024-09-03 14:53:30 -0400

    feat: rfid import enforces unique badge serials

--- test/support/helpers.ex
+++ test/support/helpers.ex
@@ -15,4 +15,30 @@ defmodule Test.Support.Helpers do
       end)
     end
   end
+
+  @doc """
+  returns a list of strings with newline at the end trimmed
+  """
+  defmacro capture_log(level \\ :info, do: expression) do
+    quote do
+      require Logger
+      original_level = Logger.level()
+      on_exit(fn -> Logger.configure(level: original_level) end)
+      Logger.configure(level: unquote(level))
+
+      {"", lines} =
+        ExUnit.CaptureLog.capture_log([colors: [enabled: false]], fn ->
+          unquote(expression)
+        end)
+        |> String.split("\n")
+        |> List.pop_at(-1)
+
+      # Reset the log level for the rest of the test.
+      # The on_exit above is needed for cleanup in case of a crash,
+      # but it runs when the test is done, not when this macro is done
+      Logger.configure(level: original_level)
+
+      lines
+    end
+  end
 end
