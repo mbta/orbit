6891372d6bc165920024b16ac06bd5b41b76a89c
Preston Mueller <pmueller@mbta.com>
2024-09-11 17:04:34 -0400

    feat: add FullStory to telemetry.ts

--- js/telemetry.ts
+++ js/telemetry.ts
@@ -1,16 +1,40 @@
 import { getMetaContent } from "./util/metadata";
+import { FullStory, init as initFullStory } from "@fullstory/browser";
+import { fullStoryIntegration } from "@sentry/fullstory";
 import * as Sentry from "@sentry/react";
 
-const sentryDsn = getMetaContent("sentryDsn");
 const environment = getMetaContent("environment");
 const release = getMetaContent("release");
+const userName = getMetaContent("userName");
 const userEmail = getMetaContent("userEmail");
 
+const fullStoryOrgId = getMetaContent("fullStoryOrgId");
+
+const SENTRY_ORG_SLUG = "mbtace";
+const sentryDsn = getMetaContent("sentryDsn");
+
+// FullStory
+if (fullStoryOrgId !== null && userEmail !== null) {
+  initFullStory({ orgId: fullStoryOrgId });
+  FullStory("setIdentity", {
+    uid: userEmail,
+    properties: {
+      displayName: userName,
+      email: userEmail,
+    },
+  });
+}
+
+// Sentry
 if (sentryDsn !== null && environment !== null) {
   Sentry.init({
     dsn: sentryDsn,
     environment,
     release: release ?? undefined,
+    integrations:
+      fullStoryOrgId !== null ?
+        [fullStoryIntegration(SENTRY_ORG_SLUG, { client: FullStory })]
+      : [],
   });
 
   if (userEmail !== null) {
