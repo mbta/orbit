f43f3ff3780638411f75c983c58bfeabb14d8331
Preston Mueller <pmueller@mbta.com>
2024-09-12 12:28:49 -0400

    fix: @lemald feedback

--- lib/orbit_web/controllers/react_app_controller.ex
+++ lib/orbit_web/controllers/react_app_controller.ex
@@ -7,7 +7,8 @@ defmodule OrbitWeb.ReactAppController do
     render(conn, :react_app,
       layout: false,
       sentry_dsn: Application.get_env(:sentry, :dsn),
-      environment: Application.get_env(:orbit, :environment)
+      environment: Application.get_env(:orbit, :environment),
+      full_story_org_id: Application.get_env(:orbit, :full_story_org_id)
     )
   end
 end
