d296b793f0b4e50d516224164c7d8f0983809922
Joe Cho <jcho1@mbta.com>
2025-04-17 12:02:13 -0400

    restrict to just Red line for now

--- lib/realtime/data.ex
+++ lib/realtime/data.ex
@@ -1,9 +1,10 @@
 defmodule Realtime.Data do
-  @type route_id :: :Blue | :Orange | :Red
+  # starting with Red line first -- don't create unnecessary structs for other lines
+  # @type route_id :: :Blue | :Orange | :Red
+  @type route_id :: :Red
 
   @spec route_id_from_string(String.t() | nil) :: Realtime.Data.route_id() | nil
   def route_id_from_string(nil), do: nil
-  # starting with Red line first -- don't create unnecessary structs for other lines
   # def route_id_from_string("Blue"), do: :Blue
   # def route_id_from_string("Orange"), do: :Orange
   def route_id_from_string("Red"), do: :Red
