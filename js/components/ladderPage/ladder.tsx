e44eef57c210269d9f764bff85bd1fedefdb2727
Joe Cho <jcho1@mbta.com>
2025-04-24 12:01:23 -0400

    conditional rendering for station spacing

--- js/components/ladderPage/ladder.tsx
+++ js/components/ladderPage/ladder.tsx
@@ -1,4 +1,5 @@
 import { Station } from "../../models/station";
+import { className } from "../../util/dom";
 import { ReactElement } from "react";
 
 export const Ladder = ({ stations }: { stations: Station[] }): ReactElement => {
@@ -7,10 +8,23 @@ export const Ladder = ({ stations }: { stations: Station[] }): ReactElement => {
 
 const StationList = ({ stations }: { stations: Station[] }): ReactElement => {
   return (
-    <ul className="w-32 border-0 border-solid">
+    <ul className="mx-auto w-32 border-x-[6px] border-solid border-gray-400">
       {stations.map((station) => {
         return (
-          <li key={station.id} className={station.spacing.toString()}>
+          <li
+            key={station.id}
+            className={className([
+              "text-center",
+              station.spacing == 28 ? "mb-28"
+              : station.spacing == 20? "mb-20"
+              : station.spacing == 14 ? "mb-14"
+              : station.spacing == 12 ? "mb-12"
+              : station.spacing == 11 ? "mb-11"
+              : station.spacing == 10 ? "mb-10"
+              : station.spacing == 8 ? "mb-8"
+              : "",
+            ])}
+          >
             {station.name}
           </li>
         );
--- js/data/stations.ts
+++ js/data/stations.ts
@@ -7,16 +7,16 @@ const buildStation = (id: string, name: string, spacing: number): Station => ({
 });
 
 export const alewifeAndrewStationsArray: Station[] = [
-  buildStation("place-alfcl", "Alewife", 18),
-  buildStation("place-davis", "Davis", 10),
-  buildStation("place-portr", "Porter", 15),
-  buildStation("place-harsq", "Harvard", 15),
-  buildStation("place-cntsq", "Central", 8),
-  buildStation("place-knncl", "Kendall", 8),
-  buildStation("place-chmnl", "Charles", 12),
-  buildStation("place-pktrm", "Park", 5),
-  buildStation("place-dwnxg", "Dwt Xing", 10),
-  buildStation("place-sstat", "South Sta", 10),
-  buildStation("place-brdwy", "Broadway", 8),
-  buildStation("place-andrw", "Andrew", 5),
+  buildStation("place-alfcl", "Alewife", 28),
+  buildStation("place-davis", "Davis", 12),
+  buildStation("place-portr", "Porter", 20),
+  buildStation("place-harsq", "Harvard", 20),
+  buildStation("place-cntsq", "Central", 10),
+  buildStation("place-knncl", "Kendall", 10),
+  buildStation("place-chmnl", "Charles", 14),
+  buildStation("place-pktrm", "Park", 8),
+  buildStation("place-dwnxg", "Dwt Xing", 11),
+  buildStation("place-sstat", "South Sta", 11),
+  buildStation("place-brdwy", "Broadway", 10),
+  buildStation("place-andrw", "Andrew", 8),
 ];
