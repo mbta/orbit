93ef3fb0c267ac68a19e6e3550780552d3606303
Joe Cho <jcho1@mbta.com>
2025-05-02 17:08:03 -0400

    add halo border to dot

--- js/components/ladderPage/train.tsx
+++ js/components/ladderPage/train.tsx
@@ -18,6 +18,7 @@ export const Train = ({
   const orientation = direction == 0 ? "right-0" : "left-0";
   return (
     <div className="relative">
+      {/* train label */}
       <div
         className={className([
           "m-1 relative flex items-center justify-center rounded-3xl w-24 h-10 font-semibold",
@@ -29,7 +30,9 @@ export const Train = ({
           extraClassName,
         ])}
       >
-        {/* line that connects to ladder */}
+        {label}
+
+        {/* line that connects to dot */}
         <div
           className={className([
             "absolute top-1/2 w-5 h-1.5 transform -translate-y-1/2",
@@ -38,21 +41,29 @@ export const Train = ({
             orientation,
           ])}
         />
-        {/* dot attached to the ladder */}
-        {/* TODO: add semi-transparent border */}
+
+        {/* dot that attaches to ladder */}
+        {/* semi-transparent border */}
         <div
           className={className([
-            "absolute rounded-full h-[16px] w-[16px]",
+            "absolute rounded-full h-[32px] w-[32px] border-8 border-opacity-35",
+            route == "Red-Braintree" ? "border-crimson" : "border-tangerine",
             direction == 0 ?
-              highlight ? "translate-x-[calc(100%+18px)]"
-              : "translate-x-[calc(100%+16px)]"
-            : highlight ? "-translate-x-[calc(100%+18px)]"
-            : "-translate-x-[calc(100%+16px)]",
-            bgColor,
+              highlight ? "translate-x-[calc(100%+10px)]"
+              : "translate-x-[calc(100%+8px)]"
+            : highlight ? "-translate-x-[calc(100%+10px)]"
+            : "-translate-x-[calc(100%+8px)]",
             orientation,
           ])}
-        />
-        {label}
+        >
+          {/* actual dot */}
+          <div
+            className={className([
+              "absolute rounded-full h-[16px] w-[16px]",
+              bgColor,
+            ])}
+          />
+        </div>
       </div>
     </div>
   );
