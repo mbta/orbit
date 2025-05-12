823082a24dd085ef6efbd991a3d784fad4d627f1
Joe Cho <jcho1@mbta.com>
2025-05-12 16:16:23 -0400

    clarify comments, DRY the index iteration

--- js/components/ladderPage/height.ts
+++ js/components/ladderPage/height.ts
@@ -8,8 +8,18 @@ export const height = (pos: VehiclePosition, stationSet: Station[]) => {
   const index = stationSet.findIndex((station) => pos.stationId === station.id);
   let height = 68;
 
+  for (let i = 0; i < index; i += 1) {
+    // spacingRatio * 32 = actual px value of bottom margin
+    // 24 = height of the station name
+    height += stationSet[i].spacingRatio * 32 + 24;
+  }
+
   if (pos.directionId === 0) {
-    // southbound
+    /* 
+    southbound: stationSet is in the order of travel --
+    we've added up the bottom margins of stations we are fully past 
+    up to (but NOT including) i. now add proportional progress towards i
+    */
 
     if (index === 0) {
       if (pos.stopStatus === StopStatus.StoppedAt) {
@@ -18,12 +28,6 @@ export const height = (pos: VehiclePosition, stationSet: Station[]) => {
         // handle case where trains InTransitTo are above the stationSet bounds
         return 20;
       }
-    }
-
-    for (let i = 0; i < index; i += 1) {
-      // spacingRatio * 32 = actual px value of bottom margin
-      // 24 = height of the station name
-      height += stationSet[i].spacingRatio * 32 + 24;
     }
 
     if (pos.stopStatus === StopStatus.InTransitTo && pos.position !== null) {
@@ -39,11 +43,12 @@ export const height = (pos: VehiclePosition, stationSet: Station[]) => {
 
     return height;
   } else {
-    // northbound
-
-    for (let i = 0; i <= index; i += 1) {
-      height += stationSet[i].spacingRatio * 32 + 24;
-    }
+    height += stationSet[index].spacingRatio * 32 + 24;
+    /*
+    northbound: stationSet is in the reverse order of of travel --
+    we've added up to (^ and now included above ^) i's bottom margins. Now "backtrack"
+    proportionally back towards i 
+    */
 
     if (pos.stopStatus === StopStatus.StoppedAt) {
       if (index === stationSet.length - 1) {
@@ -51,23 +56,23 @@ export const height = (pos: VehiclePosition, stationSet: Station[]) => {
       } else {
         return height - (stationSet[index].spacingRatio * 32 + 24);
       }
-    } else {
-      //handle case where trains InTransitTo are below stationSet bounds
-      if (index === stationSet.length - 1) {
-        return height + 40;
-      }
-
-      if (pos.position !== null) {
-        height -=
-          proportionBetweenLatLngs(
-            stationSet[index + 1].location,
-            stationSet[index].location,
-            pos.position,
-          ) *
-          (stationSet[index].spacingRatio * 32);
-      }
-
-      return height;
     }
+
+    //handle case where trains InTransitTo are below stationSet bounds
+    if (index === stationSet.length - 1) {
+      return height + 40;
+    }
+
+    if (pos.position !== null) {
+      height -=
+        proportionBetweenLatLngs(
+          stationSet[index + 1].location,
+          stationSet[index].location,
+          pos.position,
+        ) *
+        (stationSet[index].spacingRatio * 32);
+    }
+
+    return height;
   }
 };
