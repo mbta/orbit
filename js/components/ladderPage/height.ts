c342d2b199b4c507653415c4af9d070dbf040de1
Joe Cho <jcho1@mbta.com>
2025-05-14 12:47:17 -0400

    fine tune proportional progress

--- js/components/ladderPage/height.ts
+++ js/components/ladderPage/height.ts
@@ -7,6 +7,8 @@ export const height = (pos: VehiclePosition, stationSet: Station[]) => {
   const index = stationSet.findIndex((station) => pos.stationId === station.id);
   let height = 68;
 
+  // add up the bottom margins of stations up to index
+  // this will get us to the station that the vp is in relation to
   for (let i = 0; i < index; i += 1) {
     // spacingRatio * 32 = actual px value of bottom margin
     // 24 = height of the station name
@@ -14,17 +16,12 @@ export const height = (pos: VehiclePosition, stationSet: Station[]) => {
   }
 
   if (pos.directionId === 0) {
-    /* 
-    southbound: stationSet is in the order of travel --
-    we've added up the bottom margins of stations we are fully past 
-    up to (but NOT including) i. now add proportional progress towards i
-    */
-
     // handle case where trains InTransitTo are "above" the first station
     if (index === 0 && pos.stopStatus === StopStatus.InTransitTo) {
       return 20;
     }
 
+    // proportionally backtrack progress if train is still in transit towards the station
     if (pos.position !== null && pos.stopStatus === StopStatus.InTransitTo) {
       height -=
         (1 -
@@ -33,43 +30,41 @@ export const height = (pos: VehiclePosition, stationSet: Station[]) => {
             stationSet[index].location,
             pos.position,
           )) *
-        (stationSet[index - 1].spacingRatio * 32);
+        (stationSet[index - 1].spacingRatio * 32 + 24);
     }
-
     return height;
   } else {
-    height += stationSet[index].spacingRatio * 32 + 24;
-    /*
-    northbound: stationSet is in the reverse order of of travel --
-    we've added up to (^ and now included above ^) i's bottom margins. Now "backtrack"
-    proportionally back towards i 
-    */
+    if (stationSet[index].id === "place-brntn") {
+      height += 24; // extra padding because "Quincy Adams" wraps
+    }
 
     if (pos.stopStatus === StopStatus.StoppedAt) {
-      if (index === stationSet.length - 1) {
-        return height;
-      } else {
-        return height - (stationSet[index].spacingRatio * 32 + 24);
-      }
-    } else {
-      // train is still in transit, northbound
-
-      if (index === stationSet.length - 1) {
-        //handle case where trains InTransitTo are below stationSet bounds
-        return height + 40;
-      }
-
-      if (pos.position !== null) {
-        height -=
-          proportionBetweenLatLngs(
-            stationSet[index + 1].location,
-            stationSet[index].location,
-            pos.position,
-          ) *
-          (stationSet[index].spacingRatio * 32);
-      }
-
       return height;
     }
+
+    // ------ train is InTransitTo ------
+
+    // handle case where trains InTransitTo are "below" the first station
+    if (index === stationSet.length - 1) {
+      return height + 40;
+    }
+
+    /*
+    northbound stationSets are in reverse order of travel --
+    in order to backtrack progress towards the station the vp 
+    is in relation to, we must add on the current station's bottom margin
+    to travel back towards it
+    */
+    height += stationSet[index].spacingRatio * 32 + 24;
+    if (pos.position !== null) {
+      height -=
+        proportionBetweenLatLngs(
+          stationSet[index + 1].location,
+          stationSet[index].location,
+          pos.position,
+        ) *
+        (stationSet[index].spacingRatio * 32 + 24);
+    }
+    return height;
   }
 };
