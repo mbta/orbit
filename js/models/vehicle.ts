f0e4baf666a27d1bd1c6cf9b70e7069b43101d87
Andrew Dolce <adolce@mbta.com>
2025-05-29 16:34:02 -0400

    define Vehicle model to combine position/trip

---
+++ js/models/vehicle.ts
@@ -0,0 +1,31 @@
+import { TripUpdate } from "./tripUpdate";
+import { VehiclePosition } from "./vehiclePosition";
+
+export type Vehicle = {
+  vehiclePosition: VehiclePosition;
+  tripUpdate?: TripUpdate;
+};
+
+export const vehiclesFromPositionsAndTripUpdates = (
+  vehiclePositions: VehiclePosition[],
+  tripUpdates: TripUpdate[],
+): Vehicle[] => {
+  const tripUpdateById = new Map<string, TripUpdate>();
+  for (const tripUpdate of tripUpdates) {
+    const existing = tripUpdateById.get(tripUpdate.tripId);
+    if (existing) {
+      console.error("Multiple updates for trip. Overwriting");
+      console.error(existing);
+    }
+    tripUpdateById.set(tripUpdate.tripId, tripUpdate);
+  }
+
+  return vehiclePositions.map((vehiclePosition) => {
+    const tripId = vehiclePosition.trip?.tripId;
+    const tripUpdate = tripId && tripUpdateById.get(tripId);
+    return {
+      vehiclePosition,
+      ...(tripUpdate ? { tripUpdate } : {}),
+    };
+  });
+};
