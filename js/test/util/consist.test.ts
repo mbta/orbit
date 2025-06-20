4bf78a86ddb6c450292cc7314aa82f5d96fc5f28
Joe Cho <jcho1@mbta.com>
2025-06-20 12:48:30 -0400

    add test for consistDirectionalOrder function

---
+++ js/test/util/consist.test.ts
@@ -0,0 +1,76 @@
+import { consistDirectionalOrder } from "../../util/consist";
+
+describe("consistDirectionalOrder", () => {
+  test("correctly orders southbound consists", () => {
+    // already in the desired order (lead car on left)
+    const consist0 = ["1877", "1876", "1807", "1806", "1815", "1814"];
+    const fixedConsist0 = consistDirectionalOrder("1877", consist0, 0);
+
+    // consist in reverse of desired order (lead car on right)
+    const consist1 = ["1814", "1815", "1806", "1807", "1876", "1877"];
+    const fixedConsist1 = consistDirectionalOrder("1877", consist1, 0);
+
+    expect(fixedConsist0).toStrictEqual([
+      "1877",
+      "1876",
+      "1807",
+      "1806",
+      "1815",
+      "1814",
+    ]);
+    expect(fixedConsist1).toStrictEqual([
+      "1877",
+      "1876",
+      "1807",
+      "1806",
+      "1815",
+      "1814",
+    ]);
+  });
+
+  test("correctly orders northbound consists", () => {
+    // already in the desired order (lead car on right)
+    const consist0 = ["1877", "1876", "1807", "1806", "1815", "1814"];
+    const fixedConsist0 = consistDirectionalOrder("1814", consist0, 1);
+
+    // consist in reverse of desired order (lead car on left)
+    const consist1 = ["1814", "1815", "1806", "1807", "1876", "1877"];
+    const fixedConsist1 = consistDirectionalOrder("1814", consist1, 1);
+
+    expect(fixedConsist0).toStrictEqual([
+      "1877",
+      "1876",
+      "1807",
+      "1806",
+      "1815",
+      "1814",
+    ]);
+    expect(fixedConsist1).toStrictEqual([
+      "1877",
+      "1876",
+      "1807",
+      "1806",
+      "1815",
+      "1814",
+    ]);
+  });
+
+  test("logs error and returns unmodified consist if lead car not found", () => {
+    console.error = jest.fn();
+    const consist = ["1877", "1876", "1807", "1806", "1815", "1814"];
+    const fixedConsist = consistDirectionalOrder("1806", consist, 1);
+
+    expect(fixedConsist).toStrictEqual([
+      "1877",
+      "1876",
+      "1807",
+      "1806",
+      "1815",
+      "1814",
+    ]);
+    expect(console.error).toHaveBeenCalledWith(
+      "vehicle label 1806 is not the lead car in consist",
+      consist,
+    );
+  });
+});
