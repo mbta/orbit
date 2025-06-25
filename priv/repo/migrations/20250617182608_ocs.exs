a9109954e93813dd0ac230cdb0c31b02037d080d
Preston Mueller <pmueller@mbta.com>
2025-06-25 17:42:30 -0400

    feat(ocs): add ocs_trips and ocs_trains tables (#335)

---
+++ priv/repo/migrations/20250617182608_ocs.exs
@@ -0,0 +1,41 @@
+defmodule Orbit.Repo.Migrations.OCSTables do
+  use Ecto.Migration
+
+  def change do
+    create table(:ocs_trips) do
+      add(:service_date, :date, null: false)
+      add(:uid, :string, null: false)
+      add(:train_uid, :string)
+      add(:prev_uid, :string)
+      add(:next_uid, :string)
+      add(:route, :string)
+      add(:rail_line, :rail_line, null: false)
+      add(:trip_type, :string)
+      add(:scheduled_departure, :utc_datetime)
+      add(:scheduled_arrival, :utc_datetime)
+      add(:offset, :integer)
+      add(:origin_station, :string)
+      add(:destination_station, :string)
+      add(:deleted, :boolean)
+
+      timestamps()
+    end
+
+    create unique_index(:ocs_trips, [:service_date, :uid, :rail_line])
+    create index(:ocs_trips, [:service_date, :rail_line, :train_uid])
+
+    create table(:ocs_trains) do
+      add(:service_date, :date, null: false)
+      add(:uid, :string, null: false)
+      add(:rail_line, :rail_line, null: false)
+      add(:tags, :jsonb)
+      add(:cars, :jsonb)
+      add(:car_tags, :jsonb)
+      add(:deleted, :boolean)
+
+      timestamps()
+    end
+
+    create unique_index(:ocs_trains, [:service_date, :uid, :rail_line])
+  end
+end
