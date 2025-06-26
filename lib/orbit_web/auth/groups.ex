24475c52feee17d92374433af4f64943e0f8769c
Preston Mueller <pmueller@mbta.com>
2025-06-26 14:32:14 -0400

    fix: add orbit_tid_staff to fake keycloak page

--- lib/orbit_web/auth/groups.ex
+++ lib/orbit_web/auth/groups.ex
@@ -1,4 +1,6 @@
 defmodule OrbitWeb.Auth.Groups do
+  def orbit_admin, do: "orbit-admin"
   def orbit_bl_ffd, do: "orbit-bl-ffd"
+  def orbit_rl_trainstarters, do: "orbit-rl-trainstarters"
   def orbit_tid_staff, do: "orbit-tid-staff"
 end
