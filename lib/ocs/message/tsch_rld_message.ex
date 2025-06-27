50f5ad90b8b51ee70f1184f5f5eac11126a07fa1
Andrew Dolce <adolce@mbta.com>
2025-06-27 16:57:59 -0400

    feat(ocs): define tsch message structs

---
+++ lib/ocs/message/tsch_rld_message.ex
@@ -0,0 +1,12 @@
+defmodule OCS.Message.TschRldMessage do
+  @moduledoc """
+  TSCH RLD message type from OCS system
+  """
+  defstruct [:counter, :timestamp, :transitline]
+
+  @type t :: %__MODULE__{
+          counter: integer,
+          timestamp: DateTime.t(),
+          transitline: String.t()
+        }
+end
