6821cef4a333c17f10382619458ae52d84ec90a9
Andrew Dolce <adolce@mbta.com>
2025-06-30 13:06:54 -0400

    wip: ecto entity for stream state

---
+++ lib/orbit/ocs/stream/kinesis_stream_state.ex
@@ -0,0 +1,32 @@
+defmodule Orbit.Ocs.KinesisStreamState do
+  use Ecto.Schema
+
+  import Ecto.Changeset
+
+  @type t :: %__MODULE__{
+          stream_name: String.t(),
+          resume_position: String.t()
+        }
+  schema "ocs_kinesis_streams" do
+    field(:stream_name, :string)
+    field(:resume_position, :string)
+
+    timestamps()
+  end
+
+  def changeset(struct, attrs \\ %{}) do
+    struct
+    |> cast(
+      attrs,
+      [
+        :stream_name,
+        :resume_position
+      ]
+    )
+    |> unique_constraint(:stream_name)
+    |> validate_required([
+      :stream_name,
+      :resume_position
+    ])
+  end
+end
