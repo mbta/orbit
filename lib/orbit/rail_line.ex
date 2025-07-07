ff968b024b2f4b00a2c555e7ddc3bacb8ff682f0
Andrew Dolce <adolce@mbta.com>
2025-07-07 11:37:33 -0400

    feat(ocs): add module for ocs db read/write

--- lib/orbit/rail_line.ex
+++ lib/orbit/rail_line.ex
@@ -21,4 +21,14 @@ defmodule Orbit.RailLine do
   @impl true
   def dump(line) when line in [:red, :orange, :blue, :none], do: {:ok, Atom.to_string(line)}
   def dump(_), do: :error
+
+  @spec from_ocs_transitline(Orbit.Ocs.TransitLine.t()) :: t()
+  def from_ocs_transitline(transitline) do
+    case transitline do
+      :red -> :red
+      :orange -> :orange
+      :blue -> :blue
+      _ -> :none
+    end
+  end
 end
