afb148ff02c53c180e13e3df71b0cb40b5125eb0
Andrew Dolce <adolce@mbta.com>
2025-07-08 11:22:05 -0400

    fix: improve splunk formatter and add tests

--- lib/orbit/ocs/splunk_formatter.ex
+++ lib/orbit/ocs/splunk_formatter.ex
@@ -17,25 +17,41 @@ defmodule Orbit.Ocs.SplunkFormatter do
 
     formatted_type =
       case message_type do
-        "Tsch" <> suffix -> "TSCH_" <> String.upcase(suffix)
-        _ -> message_type |> String.upcase()
+        "Tsch" <> suffix ->
+          formatted_suffix = suffix |> String.replace_suffix("Message", "") |> String.upcase()
+          "TSCH_" <> formatted_suffix
+
+        _ ->
+          message_type |> String.upcase()
       end
 
     message |> Map.from_struct() |> Map.put(:type, formatted_type) |> format
   end
 
   def format(message) do
-    ["parsed_ocs_message ", to_key_value_string(message)]
+    ["parsed_ocs_message ", to_key_value_string(message, false)]
   end
 
-  @spec to_key_value_string(map()) :: iodata()
-  defp to_key_value_string(%_{} = map), do: map |> Map.from_struct() |> to_key_value_string
+  @spec to_key_value_string(map(), boolean()) :: iodata()
+  defp to_key_value_string(%_{} = map, nested),
+    do: map |> Map.from_struct() |> to_key_value_string(nested)
 
-  defp to_key_value_string(map) when is_map(map) do
-    map
-    |> Enum.sort()
-    |> Enum.map(&format_field/1)
-    |> Enum.intersperse(", ")
+  defp to_key_value_string(map, nested) when is_map(map) do
+    pairs =
+      map
+      |> Enum.sort()
+      |> Enum.map(&format_field/1)
+      |> Enum.intersperse(", ")
+
+    if nested do
+      ["{"] ++ pairs ++ ["}"]
+    else
+      pairs
+    end
+  end
+
+  defp to_key_value_string(other_value, _) do
+    inspect(other_value)
   end
 
   @spec format_field({atom(), any()}) :: iodata()
@@ -43,7 +59,18 @@ defmodule Orbit.Ocs.SplunkFormatter do
     format_field({key, DateTime.to_iso8601(value)})
   end
 
-  defp format_field({_, value}) when is_map(value), do: to_key_value_string(value)
+  defp format_field({_, %_{} = value}),
+    do: value |> Map.from_struct() |> to_key_value_string(true)
+
+  defp format_field({_, value}) when is_map(value), do: to_key_value_string(value, true)
+
+  defp format_field({key, value}) when is_list(value),
+    do: [
+      Atom.to_string(key),
+      "=[",
+      value |> Enum.map(&to_key_value_string(&1, true)) |> Enum.intersperse(", "),
+      "]"
+    ]
 
   defp format_field({key, value}), do: [Atom.to_string(key), ?=, inspect(value)]
 end
