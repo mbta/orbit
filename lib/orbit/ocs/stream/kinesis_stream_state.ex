9bf2c5167883d087d1d0d7423415c293f067c886
Andrew Dolce <adolce@mbta.com>
2025-07-09 11:33:50 -0400

    feat(ocs): define entity/table for kinesis stream states

---
+++ lib/orbit/ocs/stream/kinesis_stream_state.ex
@@ -0,0 +1,36 @@
+defmodule Orbit.KinesisStreamState do
+  use Ecto.Schema
+
+  import Ecto.Changeset
+
+  @type t :: %__MODULE__{
+          stream_name: String.t(),
+          resume_position: String.t(),
+          last_message_timestamp: DateTime.t()
+        }
+  schema "kinesis_stream_states" do
+    field(:stream_name, :string)
+    field(:resume_position, :string)
+    field(:last_message_timestamp, :utc_datetime)
+
+    timestamps()
+  end
+
+  def changeset(struct, attrs \\ %{}) do
+    struct
+    |> cast(
+      attrs,
+      [
+        :stream_name,
+        :resume_position,
+        :last_message_timestamp
+      ]
+    )
+    |> unique_constraint(:stream_name)
+    |> validate_required([
+      :stream_name,
+      :resume_position,
+      :last_message_timestamp
+    ])
+  end
+end
