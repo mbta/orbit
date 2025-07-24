033712ec16d3a3be4f2e523777de4b94dbbcdb25
Joe Cho <jcho1@mbta.com>
2025-07-24 13:33:51 -0400

    remove todos

--- js/groups.ts
+++ js/groups.ts
@@ -3,7 +3,6 @@ import { getMetaContent } from "./util/metadata";
 export const ORBIT_BL_FFD = "orbit-bl-ffd";
 export const ORBIT_RL_TRAINSTARTERS = "orbit-rl-trainstarters";
 export const ORBIT_TID_STAFF = "orbit-tid-staff";
-// TODO: confirm final name of new groups
 export const ORBIT_BL_STAKEHOLDERS = "orbit-bl-stakeholders";
 export const ORBIT_HR_STAKEHOLDERS = "orbit-hr-stakeholders";
 
--- lib/orbit_web/auth/groups.ex
+++ lib/orbit_web/auth/groups.ex
@@ -3,7 +3,6 @@ defmodule OrbitWeb.Auth.Groups do
   def orbit_bl_ffd, do: "orbit-bl-ffd"
   def orbit_rl_trainstarters, do: "orbit-rl-trainstarters"
   def orbit_tid_staff, do: "orbit-tid-staff"
-  # TODO: confirm & request creation of final group names
   def orbit_bl_stakeholders, do: "orbit-bl-stakeholders"
   def orbit_hr_stakeholders, do: "orbit-hr-stakeholders"
 end
