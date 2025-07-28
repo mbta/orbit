445e1346516441d26f0eb524ecadf27fdaedcbd7
Andrew Dolce <adolce@mbta.com>
2025-07-28 09:09:26 -0400

    feat: flag ocs trips as explicitly "departed"

--- js/models/ocs.ts
+++ js/models/ocs.ts
@@ -5,6 +5,7 @@ import z from "zod";
 export const OCSTripData = z.object({
   uid: z.string(),
   next_uid: z.string().nullable(),
+  departed: z.boolean().nullable(),
   actual_departure: z.string().nullable(),
   scheduled_departure: z.string().nullable(),
   scheduled_arrival: z.string().nullable(),
@@ -18,6 +19,7 @@ export type OCSTripData = z.infer<typeof OCSTripData>;
 export type OCSTrip = {
   uid: string;
   nextUid: string | null;
+  departed: boolean | null;
   actualDeparture: DateTime | null;
   scheduledDeparture: DateTime | null;
   scheduledArrival: DateTime | null;
@@ -30,6 +32,7 @@ export type OCSTrip = {
 export const ocsTripFromData = (data: OCSTripData): OCSTrip => ({
   uid: data.uid,
   nextUid: data.next_uid,
+  departed: data.departed,
   actualDeparture:
     data.actual_departure ? dateTimeFromISO(data.actual_departure) : null,
   scheduledDeparture:
