5ff45215ca9b737c6f77f5fdb788d152749c8c97
Joe Cho <jcho1@mbta.com>
2025-07-31 16:00:16 -0400

    feat: update redirects (#371)

--- js/components/app.tsx
+++ js/components/app.tsx
@@ -1,6 +1,12 @@
 import { reload } from "../browser";
 import { SocketProvider } from "../contexts/socketContext";
-import { ORBIT_BL_FFD, ORBIT_TID_STAFF } from "../groups";
+import {
+  ORBIT_BL_FFD,
+  ORBIT_BL_STAKEHOLDERS,
+  ORBIT_HR_STAKEHOLDERS,
+  ORBIT_TID_STAFF,
+  userHasOneOf,
+} from "../groups";
 import { paths } from "../paths";
 import { AppcuesTrackPage } from "./appcues";
 import { Header } from "./header";
@@ -51,6 +57,23 @@ const ErrorBoundary = (): ReactElement => {
   );
 };
 
+// redirect users from root "/" to different endpoints based off their usergroups
+export const UserGroupRedirects = () => {
+  if (
+    userHasOneOf([
+      ORBIT_TID_STAFF,
+      ORBIT_BL_STAKEHOLDERS,
+      ORBIT_HR_STAKEHOLDERS,
+    ])
+  ) {
+    return <Navigate to={"/landing"} />;
+  } else if (userHasOneOf([ORBIT_BL_FFD])) {
+    return <Navigate to={"/operators"} />;
+  } else {
+    return <Navigate to={"/ladder"} />;
+  }
+};
+
 const router = createBrowserRouter([
   {
     errorElement: <ErrorBoundary />,
@@ -64,8 +87,7 @@ const router = createBrowserRouter([
     children: [
       {
         path: paths.root,
-        // "/" will redirect to "/operators" for now since blue line ffd was previously located there.
-        element: <Navigate to="/operators" />,
+        element: <UserGroupRedirects />,
       },
       {
         path: paths.menu,
