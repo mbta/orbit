e2e13de745197b0e181795aceaab23a642eb1ad9
Ryan Collins <rcollins4@mbta.com>
2026-08-17 11:13:15 -0400

    test: Added end to end test for FFD sign in and the accompanying DB seed script for it

---
+++ lib/mix/tasks/seed_e2e.ex
@@ -0,0 +1,31 @@
+defmodule Mix.Tasks.SeedE2e do
+  use Mix.Task
+  @requirements ["app.start"]
+  @preferred_cli_env :dev
+
+  import Ecto.Query
+
+  alias Orbit.Employee
+  alias Orbit.Repo
+
+  @shortdoc "Seed database for end-to-end tests"
+  def run(_opts) do
+    Logger.configure(level: :notice)
+
+    employee_query =
+      from(employee in Employee, where: employee.email == "teste2eemployee@example.com")
+
+    if(!Repo.exists?(employee_query)) do
+      %Employee{
+        first_name: "Test",
+        preferred_first: "Test",
+        middle_initial: "A",
+        last_name: "Employee",
+        email: "teste2eemployee@example.com",
+        badge_number: "TEST_BADGE",
+        area: 321
+      }
+      |> Repo.insert!()
+    end
+  end
+end
