650cdd64a0abda54a91c837305cd92465bc37574
Preston Mueller <pmueller@mbta.com>
2026-08-28 17:50:30 -0400

    fix: one worker on CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,7 +11,8 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  workers: 2,
+  /* Single worker on CI, two local */
+  workers: process.env.CI ? 1 : 2,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
